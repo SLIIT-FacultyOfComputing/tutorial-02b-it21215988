90247a40919473236a0c6fe1ae471e15f0013f76
Waseem M.I.M <99674503+it21215988@users.noreply.github.com>
2022-02-23 12:37:58 +0000

    IT21215988

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,16 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int fact=1,i;
+  for(i=1;i<=no;i++)
+    {
+      fact=fact*i;
+    }
+  return fact;
+}
+long nCr(int n, int r)
+{
+  return Factorial(n)/Factorial(r)*(Factorial(n-r));
+}
